be21a9533cd98241f104f01e1eeaf0c1d9ef375f
Rainys <citromincer@gmail.com>
2024-10-12 17:09:06 +0300

    GenerateProject-Windows script no longer requires pressing enter 🔥

--- GenerateProject-Windows.bat
+++ GenerateProject-Windows.bat
@@ -7,19 +7,15 @@ echo [3] Exit
 echo.
 
 :choice
-set /p CHOICE="Press [1], [2], or [3] to choose (default is 1): "
-if "%CHOICE%"=="" set CHOICE=1
-
-if "%CHOICE%"=="1" (
-    call tools\premake\Windows\premake5.exe vs2022
-) else if "%CHOICE%"=="2" (
-    call tools\premake\Windows\premake5.exe gmake2
-) else if "%CHOICE%"=="3" (
+choice /c 123 /n /m "Press [1], [2], or [3] to choose (default is 1): "
+if errorlevel 3 (
     echo Exiting...
     exit /b
-) else (
-    echo Invalid choice. Please try again.
-    goto choice
+) else if errorlevel 2 (
+    call tools\premake\Windows\premake5.exe gmake2
+) else if errorlevel 1 (
+    call tools\premake\Windows\premake5.exe vs2022
 )
 
+REM :: echo Done.
 PAUSE
