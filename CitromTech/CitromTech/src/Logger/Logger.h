e1b01116c48b77d1bd937945b3131fc9ec87afb9
Rainys <citromincer@gmail.com>
2024-10-19 23:22:50 +0300

    Started work on the logger

--- CitromTech/CitromTech/src/Logger/Logger.h
+++ CitromTech/CitromTech/src/Logger/Logger.h
@@ -1,8 +1,36 @@
 #pragma once
 
-#include <format>
+#include "Core.h"
+
+//#include <format>
+
+/*
+* Old Logger:
+	Info = variables/information - Gray
+	Log = something somewhat important got executed - Green
+	Warn = something that shouldn't happen but the process can still run - Yellow
+	Error = something that shouldn't happen but the application can still run - Purple
+	Fatal/Critical/Severe = something that shouldn't happen and the application must close or already has - Red
+
+* New Logger:
+	Trace = track the program execution - Blue (most likely dark)
+	Info = important information that is supposed to happen under normal conditions - Gray
+	Debug = something - maybe green?
+	Warn = something that shouldn't happen but the process can still run - Yellow
+	Error = something that shouldn't happen but the application can still run - Purple
+	Fatal/Severe/Critical = something that shouldn't happen and the application must close or already has - Red
+*/
+#define CT_TRACE(...)	//__VA_ARGS__
 
 namespace Citrom
 {
-
+	class Logger
+	{
+	public:
+		// member functions for the logger
+	private:
+		//static const char* ParseFormat(const char* log, va_list args, const char* preMessage);
+	private:
+		// static logger instances
+	};
 }
