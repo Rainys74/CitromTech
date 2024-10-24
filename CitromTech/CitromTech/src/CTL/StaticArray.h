5cfcf30f93894bdf1985fad1e34b6c0fbb45dd53
Rainys <citromincer@gmail.com>
2024-10-24 19:02:11 +0300

    File location fixes after XCode.

--- CitromTech/CitromTech/src/CTL/StaticArray.h
+++ CitromTech/CitromTech/src/CTL/StaticArray.h
@@ -10,23 +10,23 @@ namespace CTL
     class Array
     {
     public:
-        constexpr size_t Size() const { return S;}
-        
+        constexpr size_t Size() const { return S; }
+
         // Operator overloading
-        FORCE_INLINE T& operator[](size_t index) { return m_Data[index];}
-        FORCE_INLINE const T& operator[](size_t index) const { return m_Data[index];}
-        
-        FORCE_INLINE T* Data() {return m_Data;}
-        FORCE_INLINE const T* Data() const {return m_Data;}
-        
+        FORCE_INLINE T& operator[](size_t index) { return m_Data[index]; }
+        FORCE_INLINE const T& operator[](size_t index) const { return m_Data[index]; }
+
+        FORCE_INLINE T* Data() { return m_Data; }
+        FORCE_INLINE const T* Data() const { return m_Data; }
+
         // Iterators
         FORCE_INLINE T* begin() { return &m_Data[0]; }
         FORCE_INLINE const T* begin() const { return &m_Data[0]; }
         FORCE_INLINE T* end() { return &m_Data[S]; }
         FORCE_INLINE const T* end() const { return &m_Data[S]; }
-        
+
         //void Zero() {memset(&m_Data, 0, S * sizeof(T));}
     private:
         T m_Data[S];
-    }
+    };
 }
