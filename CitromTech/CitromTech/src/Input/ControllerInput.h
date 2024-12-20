ea3fe5a988bc80be5ee47f60edb7b9bca5805103
Rainys <citromincer@gmail.com>
2024-12-20 13:34:50 +0200

    Gamepad enums in ControllerInput.h

---
+++ CitromTech/CitromTech/src/Input/ControllerInput.h
@@ -0,0 +1,50 @@
+#pragma once
+
+#include "Core.h"
+
+namespace Citrom::Input
+{
+    // Gamepads
+    enum class ControllerAxes
+    {
+        Null,
+
+        LeftStickHorizontal,
+        LeftStickVertical,
+
+        RightStickHorizontal,
+        RightStickVertical,
+
+        LeftTrigger, // L2
+        RightTrigger, // R2
+
+        Count
+    };
+
+    enum class ControllerButtons
+    {
+        Null,
+
+        // Xbox = Playstation = Labels
+        Top, // Y = Triangle = Alt-Action
+        Left, // X = Square = Special
+        Bottom, // A = X/Cross = Action
+        Right, // B = Circle/O = Back
+
+        LeftShoulder, // LB = L1
+        RightShoulder, // RB = R1
+
+        DPadUp,
+        DPadLeft,
+        DPadBottom,
+        DPadRight,
+
+        LeftStickButton, // L3
+        RightStickButton, // R3
+
+        Options, // Menu
+        //Share, // View
+
+        Count
+    };
+}
