dbad4ea39db5f8d82dabedb3018f520e93258e91
Rainys <citromincer@gmail.com>
2025-01-11 22:30:37 +0200

    Shaders now correctly get compiled.. i think.

--- CitromTech/CitromTech/src/Renderer/RenderAPI/DirectX11/DX11ShaderCompiler.cpp
+++ CitromTech/CitromTech/src/Renderer/RenderAPI/DirectX11/DX11ShaderCompiler.cpp
@@ -11,6 +11,8 @@
 
 #include "CTL/DArray.h"
 
+#define DXCallErrorBlobHR(x) DXCallHR(x); if (errorBlob) {MessageBoxA(nullptr, (char*)errorBlob->GetBufferPointer(), "D3DCompileFromFile Error Blob!", MB_ICONERROR);}
+
 namespace Citrom::ShaderCompiler::DX11
 {
 	struct ShaderObj
@@ -18,7 +20,7 @@ namespace Citrom::ShaderCompiler::DX11
 		std::string name;
 		RenderAPI::ShaderType type;
 		//void* shaderBlob;
-		ID3DBlob* shaderBlob;
+		ID3DBlob* shaderBlob = nullptr;
 	};
 
 	static CTL::DArray<ShaderObj> CompileShadersToBlobs(const std::string shaderPaths[], const uint32 pathCount)
@@ -36,7 +38,53 @@ namespace Citrom::ShaderCompiler::DX11
 
 				if (entry.path().extension() == ".hlsl")
 				{
+					CT_CORE_TRACE("Found HLSL Shader in {}", entry.path().string());
+
 					ShaderObj vertexShaderObject;
+					ShaderObj pixelShaderObject;
+
+					vertexShaderObject.type = RenderAPI::ShaderType::Vertex;
+					pixelShaderObject.type = RenderAPI::ShaderType::Fragment;
+
+					HRESULT hr;
+
+					ID3DBlob* errorBlob = nullptr;
+
+					if (entry.path().stem().string().find("_vs") != std::string::npos)
+					{
+						vertexShaderObject.name = entry.path().stem().string();
+						CT_CORE_TRACE("Found Separated HLSL Vertex Shader {}", vertexShaderObject.name);
+
+						DXCallErrorBlobHR(D3DCompileFromFile(entry.path().wstring().c_str(), nullptr, nullptr, "main", "vs_5_0", NULL, NULL, &vertexShaderObject.shaderBlob, &errorBlob));
+					}
+					else if (entry.path().stem().string().find("_fs") != std::string::npos)
+					{
+						pixelShaderObject.name = entry.path().stem().string();
+						CT_CORE_TRACE("Found Separated HLSL Pixel Shader {}", pixelShaderObject.name);
+
+						DXCallErrorBlobHR(D3DCompileFromFile(entry.path().wstring().c_str(), nullptr, nullptr, "main", "ps_5_0", NULL, NULL, &pixelShaderObject.shaderBlob, &errorBlob));
+					}
+					else
+					{
+						vertexShaderObject.name = pixelShaderObject.name = entry.path().stem().string();
+						CT_CORE_TRACE("Found Combined (Vertex/Pixel) HLSL Shader {}", vertexShaderObject.name);
+
+						DXCallErrorBlobHR(D3DCompileFromFile(entry.path().wstring().c_str(), nullptr, nullptr, "vsmain", "vs_5_0", NULL, NULL, &vertexShaderObject.shaderBlob, &errorBlob));
+						DXCallErrorBlobHR(D3DCompileFromFile(entry.path().wstring().c_str(), nullptr, nullptr, "psmain", "ps_5_0", NULL, NULL, &pixelShaderObject.shaderBlob, &errorBlob));
+					}
+
+					if (vertexShaderObject.shaderBlob)
+					{
+						CT_CORE_INFO("Successfully Compiled DX11 Vertex Shader {}", vertexShaderObject.name);
+						shaderObjects.PushBack(vertexShaderObject);
+					}
+					if (pixelShaderObject.shaderBlob)
+					{
+						CT_CORE_INFO("Successfully Compiled DX11 Pixel Shader {}", pixelShaderObject.name);
+						shaderObjects.PushBack(pixelShaderObject);
+					}
+
+					/*ShaderObj vertexShaderObject;
 					ShaderObj pixelShaderObject;
 
 					vertexShaderObject.name = pixelShaderObject.name = entry.path().stem().string();
@@ -47,16 +95,11 @@ namespace Citrom::ShaderCompiler::DX11
 					HRESULT hr;
 
 					ID3DBlob* errorBlob = nullptr;
-					DXCallHR(D3DCompileFromFile(entry.path().wstring().c_str(), nullptr, nullptr, "vsmain", "vs_5_0", NULL, NULL, &vertexShaderObject.shaderBlob, &errorBlob));
-					DXCallHR(D3DCompileFromFile(entry.path().wstring().c_str(), nullptr, nullptr, "psmain", "ps_5_0", NULL, NULL, &pixelShaderObject.shaderBlob, &errorBlob));
-
-					#ifndef CT_OPTIMIZATION
-					if (errorBlob)
-						MessageBoxA(nullptr, (char*)errorBlob->GetBufferPointer(), "D3DCompileFromFile Error Blob!", MB_ICONERROR);
-					#endif
+					DXCallErrorBlobHR(D3DCompileFromFile(entry.path().wstring().c_str(), nullptr, nullptr, "vsmain", "vs_5_0", NULL, NULL, &vertexShaderObject.shaderBlob, &errorBlob));
+					DXCallErrorBlobHR(D3DCompileFromFile(entry.path().wstring().c_str(), nullptr, nullptr, "psmain", "ps_5_0", NULL, NULL, &pixelShaderObject.shaderBlob, &errorBlob));
 
 					shaderObjects.PushBack(vertexShaderObject);
-					shaderObjects.PushBack(pixelShaderObject);
+					shaderObjects.PushBack(pixelShaderObject);*/
 				}
 			}
 		}
@@ -70,17 +113,20 @@ namespace Citrom::ShaderCompiler::DX11
 		CTL::DArray<ShaderObj> shaderObjects = CompileShadersToBlobs(shaderPaths, pathCount);
 
 		for (const ShaderObj& shaderObj : shaderObjects)
-		{
-			std::string shaderTypePrefix("_xx");
-			switch (shaderObj.type)
+		{	
+			std::string shaderTypePrefix(""); //("_xx");
+			if (shaderObj.name.find("_vs") == std::string::npos && shaderObj.name.find("_fs") == std::string::npos)
 			{
-				case RenderAPI::ShaderType::Vertex:
-					shaderTypePrefix = "_vs";
-					break;
-				case RenderAPI::ShaderType::Fragment:
-					shaderTypePrefix = "_fs";
-					break;
-				default: CT_CORE_ASSERT(false, "Invalid Shader Type!"); break;
+				switch (shaderObj.type)
+				{
+					case RenderAPI::ShaderType::Vertex:
+						shaderTypePrefix = "_vs";
+						break;
+					case RenderAPI::ShaderType::Fragment:
+						shaderTypePrefix = "_fs";
+						break;
+					default: CT_CORE_ASSERT(false, "Invalid Shader Type!"); break;
+				}
 			}
 
 			// DXBC - DirectX Byte-Code; CSO - Compiled Shader Object; DXIL - DirectX Intermediate Language;
@@ -91,6 +137,8 @@ namespace Citrom::ShaderCompiler::DX11
 			file.close();
 			
 			shaderObj.shaderBlob->Release();
+
+			CT_CORE_INFO("Successfully wrote Shader {} to file: {}", shaderObj.name, outPath + shaderObj.name + shaderTypePrefix + ".dxbc");
 		}
 	}
 }
